c02d3dc8205a8ae0acecb8cdbe33a7159eebe14f
Marcus Adamantidis Grandien <127440663+twicon@users.noreply.github.com>
2023-11-22 15:09:39 +0100

    add card shuffle function

--- app.js
+++ app.js
@@ -1,3 +1,28 @@
+function createDeck() {
+    const baseImagePaths = [
+        {value: 1, img: 'images/bild1.webp'},
+        {value: 2, img: 'images/bild2.webp'},
+        {value: 3, img: 'images/bild3.webp'},
+        {value: 4, img: 'images/bild4.webp'},
+        {value: 5, img: 'images/bild5.webp'},
+        {value: 6, img: 'images/bild6.webp'}
+    ];
+
+    let deck = baseImagePaths.concat(baseImagePaths);
+
+    return shuffle(deck);
+}
+
+function shuffle(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]]; // Byt plats på elementen
+    }
+    return array;
+}
+
+
+
 const main = document.querySelector('main');
 
 function createCardsObject(boardBlueprint) {
@@ -6,7 +31,7 @@ function createCardsObject(boardBlueprint) {
 	for (const card of boardBlueprint) {
 		const cardFront = document.createElement('p');
 		cardFront.classList.add('card__front');
-		cardFront.appendChild(document.createTextNode(card));
+		cardFront.appendChild(document.createTextNode(card.value));
 
 		const cardElement = document.createElement('article');
 		cardElement.classList.add('card');
@@ -23,8 +48,8 @@ function paintCardsOnBoard(cardsObject) {
 		main.appendChild(card.cardElement);
 	}
 }
-
-const cardsObject = createCardsObject([1, 3, 5, 3, 7, 11, 11, 7]);
+const deckBlueprint = createDeck();
+const cardsObject = createCardsObject(deckBlueprint);
 
 paintCardsOnBoard(cardsObject);
 
