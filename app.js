0d5b47884475becfe3726dd719720a4725e02efb
marcus grandien <marcus.grandien@gmail.com>
2023-11-24 16:35:14 +0100

    user form intro

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ import { startGame } from './src/game.js';
 setupDeck();
 setupScoreListeners();
 startGame();
+
+document.querySelector('#playerForm').addEventListener('submit', function(event){
+    event.preventDefault();
+    var player1 = document.querySelector('#player1').value;
+    var player2 = document.querySelector('#player2').value;
+
+    console.log("Player one: " + player1);
+    console.log("Player two: " + player2);
+
+    if (player1 != '' && player2 != '') {
+        console.log('START GAME');
+    } else {
+        console.log('Please enter names for both players');
+    }
+})
