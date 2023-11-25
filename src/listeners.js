07b6d62caac4140f4bb954df8b71f1ebef0a36ec
Moosees <linus.almgren@gmail.com>
2023-11-25 22:21:00 +0100

    add state update function and dom update event listener for player names

--- src/listeners.js
+++ src/listeners.js
@@ -1,12 +1,28 @@
 import { incrementScoreForPlayer } from './store/state.js';
 
-export function setupScoreListeners() {
-	const scoreElement = document.querySelector('.score');
+export function setupScoreboardListener() {
+	const scoreElement = document.querySelector('.scoreboard');
 
 	// if score in state changes the html elements will update to reflect this
-	scoreElement.addEventListener('updateScore', function (e) {
-		scoreElement.querySelector('.score__p1').innerText = e.detail.player1;
-		scoreElement.querySelector('.score__p2').innerText = e.detail.player2;
+	scoreElement.addEventListener('scoreboard', function (e) {
+		console.log({scoreboardUpdate: e.detail})
+		if (e.detail.score?.player1) { // only update elements if needed
+			scoreElement.querySelector('.scoreboard__score-p1').innerText =
+				e.detail.score.player1;
+		}
+		if (e.detail.score?.player2) {
+			scoreElement.querySelector('.scoreboard__score-p2').innerText =
+				e.detail.score.player2;
+		}
+
+		if (e.detail.names?.player1) {
+			scoreElement.querySelector('.scoreboard__name-p1').innerText =
+				e.detail.names.player1;
+		}
+		if (e.detail.names?.player1) {
+			scoreElement.querySelector('.scoreboard__name-p2').innerText =
+				e.detail.names.player2;
+		}
 	});
 }
 
