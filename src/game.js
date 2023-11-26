49aa1420f51312f7b38e98c1570ba38b779501c4
Moosees <linus.almgren@gmail.com>
2023-11-26 20:01:44 +0100

    add basic logig for gameloop

--- src/game.js
+++ src/game.js
@@ -1,3 +1,16 @@
-export function startGame() { // placeholder function
+import { takeTurn } from './turn.js';
+
+export function startGame() {
+	// placeholder function
 	console.log('game started');
+	let gameIsRunning = true;
+	let currentPlayer = 1;
+	let turnsCompleted = 0;
+
+	while (gameIsRunning) {
+		takeTurn(currentPlayer);
+
+		currentPlayer = (currentPlayer % 2) + 1; // alternate between 1 and 2
+		if (++turnsCompleted === 4) gameIsRunning = false;
+	}
 }
