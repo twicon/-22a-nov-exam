569be01b6c6865642f8b7494c5d6a961502a54a8
Moosees <linus.almgren@gmail.com>
2023-11-28 22:28:16 +0100

    move player name form controls to new file

--- app.js
+++ app.js
@@ -1,25 +1,5 @@
-import { setupDeck } from './src/deck.js';
-import { startGame } from './src/game.js';
 import { setupScoreboardListener } from './src/listeners.js';
-import { updatePlayerNames } from './src/store/state.js';
+import { openGameOptions } from './src/new-game.js';
 
-setupDeck();
 setupScoreboardListener();
-startGame();
-
-document
-	.querySelector('#playerForm')
-	.addEventListener('submit', function (event) {
-		event.preventDefault();
-		var player1 = document.querySelector('#player1').value;
-		var player2 = document.querySelector('#player2').value;
-
-		console.log('Player one: ' + player1);
-		console.log('Player two: ' + player2);
-
-		if (player1 != '' && player2 != '') {
-			updatePlayerNames(player1, player2);
-		} else {
-			console.log('Please enter names for both players');
-		}
-	});
+openGameOptions();
