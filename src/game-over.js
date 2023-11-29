09093f57dec9bc4f2325fe60b92ecdeab29ddc01
marcus grandien <marcus.grandien@gmail.com>
2023-11-29 19:27:29 +0100

    new game btn

--- src/game-over.js
+++ src/game-over.js
@@ -2,8 +2,8 @@ export function openGameOver() {
 	const backdrop = document.querySelector('.backdrop');
 	const gameOver = document.querySelector('.game-over');
 
-	backdrop.style.display = 'flex';
-	gameOver.style.display = 'flex';
+	backdrop.style.display = 'none';
+	gameOver.style.display = 'none';
 
 	// add button with click event listener
 	// on click:
